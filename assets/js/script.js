833755a40f94949879986fc9710abed9bbc80794
k1te <kmiller343@gmail.com>
2020-09-29 01:16:18 -0500

    successfully coded interval timer for quiz/transition to first question once begin has been clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,28 @@
-const quizContainer = document.getElementById("#quiz");
-const startQuiz = document.getElementById("#begin");
+const quizContainer = document.querySelector("#quiz");
+const startQuiz = document.querySelector("#begin");
+const timerEl = document.querySelector("#timer");
+const quest = document.querySelector("#question");
+const answr = document.querySelector("#answer");
+const questions = [
+    "placeholder",
+    "placeholder2",
+];
+const answers = [
+    "answer1",
+    "answer2",
+];
+
+let count = 60;
+
+const beginQuiz = () => { 
+    var timerInterval = setInterval(function() {
+        count--;
+        timerEl.textContent = `Timer: ${count}`;
+    }, 1000);
+    quest.textContent = questions[0];
+    answr.textContent = answers[0];
+
+}
+
+
+startQuiz.onclick = function() {beginQuiz()};
