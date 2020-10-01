783b028c48fae9a94af86ce50ea7b81406526a27
k1te <kmiller343@gmail.com>
2020-09-30 19:21:48 -0500

    added click event listener for user choice, alerts correct/incorrect and increases/decreases timer accordingly. Score and loop still not working correctly.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,15 @@ const quizContainer = document.querySelector("#quiz");
 const startQuiz = document.querySelector("#begin");
 const timerEl = document.querySelector("#timer");
 const quest = document.querySelector("#question");
-const choiceA = document.querySelector("#a");
-const choiceB = document.querySelector("#b");
-const choiceC = document.querySelector("#c");
-const choiceD = document.querySelector("#d");
+const choiceA = document.querySelector("#choiceA");
+const choiceB = document.querySelector("#choiceB");
+const choiceC = document.querySelector("#choiceC");
+const choiceD = document.querySelector("#choiceD");
+const buttonA = document.querySelector("#a");
+const buttonB = document.querySelector("#b");
+const buttonC = document.querySelector("#c");
+const buttonD = document.querySelector("#d");
+const scoreDisp = document.querySelector("#score");
 
 // Questions, Answers, and Global Variables
 const quizQuestions = [
@@ -68,6 +73,7 @@ console.log(quizQuestions[0].correctAnswer);
 // Variables
 let count = 90;
 let score = 0;
+let currentQuestion;
 let timer;
 
 // Functions
@@ -79,21 +85,49 @@ const beginQuiz = () => {
   timer = setInterval(renderTimer, 1000);
 }
 
-const renderQuestion = () => {
-  quest.innerHTML = "<p>"+ quizQuestions[0].question +"</p>";
-  choiceA.innerHTML = "<button type='radio'>"+ quizQuestions[0].answers.a +"</button>";
-  choiceB.innerHTML = "<button type='radio'>"+ quizQuestions[0].answers.b +"</button>";
-  choiceC.innerHTML = "<button type='radio'>"+ quizQuestions[0].answers.c +"</button>";
-  choiceD.innerHTML = "<button type='radio'>"+ quizQuestions[0].answers.d +"</button>";
+function renderQuestion() { // renders the next question to the question container
+  quizContainer.addEventListener("click", function(event) {
+    console.log(event.target.id)
+    if (event.target.id === quizQuestions[0].correctAnswer){
+      alert("Correct!");
+      score = score + 100;
+      count = count + 10;
+    } else {
+      alert("Incorrect! Try again!");
+      score = score - 100;
+      count = count -10;
+    }
+  });
+  
+  for (i = 0; i < quizQuestions.length; i++) {
+    quest.innerHTML = "<p>"+ quizQuestions[0].question +"</p>";
+    choiceA.innerHTML = "<button type='radio' id='a'>"+ quizQuestions[0].answers.a +"</button>";
+    choiceB.innerHTML = "<button type='radio' id='b'>"+ quizQuestions[0].answers.b +"</button>";
+    choiceC.innerHTML = "<button type='radio' id='c'>"+ quizQuestions[0].answers.c +"</button>";
+    choiceD.innerHTML = "<button type='radio' id='d'>"+ quizQuestions[0].answers.d +"</button>";
+    scoreDisp.innerHTML = "<h3>"+ 'Score: '+ score +"</h3>";
+
+ 
+  }
+
+  
+  
 }
 
 
-// OnClick
+
+
+
+
+
+
+// OnClick Begin
 startQuiz.onclick = function () {
   welcomeContainer.style.display = "none";
   renderQuestion();
   quizContainer.style.display = "block";
   renderTimer();
   timer = setInterval(renderTimer,1000);
-  
 };
+
+
