bc147c0e48ff6d7340a6e9c45d4217f7c3f5d110
k1te <kmiller343@gmail.com>
2020-10-01 00:08:38 -0500

    implemented nextQuestion function/score tracker

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,27 +4,31 @@ const quizContainer = document.querySelector("#quiz");
 const startQuiz = document.querySelector("#begin");
 const timerEl = document.querySelector("#timer");
 const quest = document.querySelector("#question");
-const choiceA = document.querySelector("#choiceA");
-const choiceB = document.querySelector("#choiceB");
-const choiceC = document.querySelector("#choiceC");
-const choiceD = document.querySelector("#choiceD");
+// const choiceA = document.querySelector("#choiceA");
+// const choiceB = document.querySelector("#choiceB");
+// const choiceC = document.querySelector("#choiceC");
+// const choiceD = document.querySelector("#choiceD");
 const buttonA = document.querySelector("#a");
 const buttonB = document.querySelector("#b");
 const buttonC = document.querySelector("#c");
 const buttonD = document.querySelector("#d");
 const scoreDisp = document.querySelector("#score");
+// const buttonA = document.querySelector("#choiceA");
+const buttonSec = document.querySelector("#buttons");
+
+
 
 // Questions, Answers, and Global Variables
 const quizQuestions = [
     {
-        question: "Who holds the MLB record for the most career homeruns?",
+        question: "Who holds the MLB record for the most career home runs?",
         answers: {
             a: "Mark McGwire",
             b: "Willie Mays",
             c: "Barry Bonds",
             d: "Babe Ruth",
         },
-        correctAnswer: "d"
+        correctAnswer: "c"
     },
     {
         question: "Who holds the MLB record for the most career hits?",
@@ -73,11 +77,11 @@ console.log(quizQuestions[0].correctAnswer);
 // Variables
 let count = 90;
 let score = 0;
-let currentQuestion;
+let currentQuestion = 0;
 let timer;
 
 // Functions
-const renderTimer = () => {
+const renderTimer = () =>  {
   count--;
   timerEl.textContent = `Timer: ${count}`;
 }
@@ -85,49 +89,79 @@ const beginQuiz = () => {
   timer = setInterval(renderTimer, 1000);
 }
 
-function renderQuestion() { // renders the next question to the question container
-  quizContainer.addEventListener("click", function(event) {
-    console.log(event.target.id)
-    if (event.target.id === quizQuestions[0].correctAnswer){
+function checkAnswer() {
+  buttonSec.addEventListener("click", function(event) {
+    if (event.target.tagName === "BUTTON" && event.target.id === quizQuestions[0].correctAnswer) {
       alert("Correct!");
-      score = score + 100;
-      count = count + 10;
-    } else {
-      alert("Incorrect! Try again!");
-      score = score - 100;
-      count = count -10;
+      count += 10;
+      currentQuestion+=1;
+      console.log(currentQuestion);
+      nextQuestion(currentQuestion);
     }
-  });
-  
+  })
+}
+
+function nextQuestion() {
   for (i = 0; i < quizQuestions.length; i++) {
-    quest.innerHTML = "<p>"+ quizQuestions[0].question +"</p>";
-    choiceA.innerHTML = "<button type='radio' id='a'>"+ quizQuestions[0].answers.a +"</button>";
-    choiceB.innerHTML = "<button type='radio' id='b'>"+ quizQuestions[0].answers.b +"</button>";
-    choiceC.innerHTML = "<button type='radio' id='c'>"+ quizQuestions[0].answers.c +"</button>";
-    choiceD.innerHTML = "<button type='radio' id='d'>"+ quizQuestions[0].answers.d +"</button>";
+    quest.innerHTML = "<p>"+ quizQuestions[currentQuestion].question +"</p>";
+    buttonA.textContent = quizQuestions[currentQuestion].answers.a;
+    buttonB.textContent = quizQuestions[currentQuestion].answers.b;
+    buttonC.textContent = quizQuestions[currentQuestion].answers.c;
+    buttonD.textContent = quizQuestions[currentQuestion].answers.d;
     scoreDisp.innerHTML = "<h3>"+ 'Score: '+ score +"</h3>";
-
- 
   }
+}
 
-  
-  
+function renderQuestion() { // renders the next question to the question container
+  // for (i = 0; i < quizQuestions.length; i++) {
+    let i = currentQuestion;
+    quest.innerHTML = "<p>"+ quizQuestions[i].question +"</p>";
+    buttonA.textContent = quizQuestions[i].answers.a;
+    buttonB.textContent = quizQuestions[i].answers.b;
+    buttonC.textContent = quizQuestions[i].answers.c;
+    buttonD.textContent = quizQuestions[i].answers.d;
+    scoreDisp.innerHTML = "<h3>"+ 'Score: '+ score +"</h3>";
+    checkAnswer();
 }
 
 
 
 
 
+// console.log(choiceEls);
+    // quizContainer.addEventListener("click", function(event) { // Not working as intended, clicks anywhere inside of the quizContainer are being captured as incorrect
+    //   console.log(event.target.id)
+    //   if (event.target.id === quizQuestions[0].correctAnswer){
+    //     alert("Correct!");
+    //     score = score + 100;
+    //     count = count + 10;  
+    //     // return renderQuestion;
+    //   } else {
+    //     alert("Incorrect! Try again!");
+    //     score = score - 100;
+    //     count = count -10;
+    //   }
+    // });
+
+  
+  
+// }
 
 
 
-// OnClick Begin
+
+
+
+
+
+// OnClick/Event Listeners
 startQuiz.onclick = function () {
   welcomeContainer.style.display = "none";
-  renderQuestion();
+  renderQuestion(0);
   quizContainer.style.display = "block";
   renderTimer();
-  timer = setInterval(renderTimer,1000);
+  beginQuiz();
 };
+
 
 
